2619da74a9d0bf45ceed1c55ff28234d40443c6f
Jeremy Tammik <jeremy.tammik@autodesk.com>
2020-05-13 15:43:13 +0200

    incremented build version number

--- XtraVb/My Project/AssemblyInfo.vb
+++ XtraVb/My Project/AssemblyInfo.vb
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("2021.0.0.0")>
-<Assembly: AssemblyFileVersion("2021.0.0.0")>
+<Assembly: AssemblyVersion("2021.0.0.1")>
+<Assembly: AssemblyFileVersion("2021.0.0.1")>
 <Assembly: ComVisibleAttribute(False)>
